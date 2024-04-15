104a4ddf84b12214ef7a013cacad98e940568a75
Josue-Alberto <101291362+Josue-Alberto@users.noreply.github.com>
2024-04-15 16:22:46 -0400

    aplicacion completa, con apk compilado, y lista para instalar y usar

--- lib/screens/acercade.dart
+++ lib/screens/acercade.dart
@@ -7,6 +7,7 @@ class AcercadeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange[900],
         title: Text('Acerca de'),
       ),
       body: SingleChildScrollView(
